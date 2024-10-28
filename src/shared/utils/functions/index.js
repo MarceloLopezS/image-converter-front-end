b3e774df9fc914f0bb0132ee9e932b6ee5e6d60b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-10-28 16:30:59 -0500

    add: Required utils

---
+++ src/shared/utils/functions/index.js
@@ -0,0 +1,19 @@
+import { DARK, LIGHT, PREFERS_COLOR_SCHEME_DARK, THEME } from "../constants"
+
+export const setLocalStorageItem = (key, item) => {
+  return localStorage.setItem(key, JSON.stringify(item))
+}
+
+export const getLocalStorageItem = (key) => {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+export const getPreferedColorScheme = () => {
+  const localPreference = getLocalStorageItem(THEME)
+
+  if (localPreference) return localPreference
+
+  return window.matchMedia(PREFERS_COLOR_SCHEME_DARK).matches
+    ? DARK
+    : LIGHT
+}
