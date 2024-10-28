15ab88a08f4fea3dd7d80c9b6353bb7810c8adea
Marcelo Isidoro Lopez Silva <101422491+MarceloLopezS@users.noreply.github.com>
2024-10-28 16:33:59 -0500

    add: Theme toggler component with required SVG components (#17)

---
+++ src/shared/ui/ThemeToggler/index.jsx
@@ -0,0 +1,19 @@
+import { useStoreData, dispatch } from "../../state/store"
+import { TOGGLE_THEME } from "../../state/config/actions"
+import { DARK } from "../../utils/constants"
+import SunSVG from "../SVGs/Sun"
+import MoonSVG from "../SVGs/Moon"
+
+const ThemeToggler = ({ ...attributes }) => {
+	const theme = useStoreData(state => state.theme)
+	const dispatchAction = () => dispatch({ type: TOGGLE_THEME })
+
+	return (
+		<button type="button" onClick={dispatchAction} {...attributes}>
+			<span className="visually-hidden">Toggle Theme</span>
+			{theme === DARK ? <SunSVG /> : <MoonSVG />}
+		</button>
+	)
+}
+
+export default ThemeToggler
