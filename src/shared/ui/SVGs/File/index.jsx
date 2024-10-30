f4caa447af37f480e3f777786d827798aaddeca7
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-10-30 16:11:54 -0500

    add: File svg

---
+++ src/shared/ui/SVGs/File/index.jsx
@@ -0,0 +1,24 @@
+const FileSVG = ({ ...attributes }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...attributes}
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M14 3v4a1 1 0 0 0 1 1h4" />
+			<path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+			<path d="M12 11l0 6" />
+			<path d="M9 14l6 0" />
+		</svg>
+	)
+}
+
+export default FileSVG
