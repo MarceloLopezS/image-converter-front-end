f8c8e3fac4b0f4b55a1aeb3885e55feec30f94d1
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-10-30 18:02:33 -0500

    update: replace normal paths with path aliases

--- src/widgets/FileControlSection/ui/ImageInputContainer/index.jsx
+++ src/widgets/FileControlSection/ui/ImageInputContainer/index.jsx
@@ -1,27 +1,27 @@
 import { useRef } from "react"
-import FileInput from "../../../../shared/ui/FileInput"
-import FileSVG from "../../../../shared/ui/SVGs/File"
+import FileInput from "@shared/ui/FileInput"
+import FileSVG from "@shared/ui/SVGs/File"
 import styles from "./ui/styles.module.css"
 
 const ACCEPTED_FILES = ["image/png", "image/jpeg", "image/webp"]
 
 const ImageInputContainer = () => {
-  const inputRef = useRef(null)
+	const inputRef = useRef(null)
 
-  return (
-    <section className={styles["image-control"]}>
-      <div className={styles["image-control__file-container"]}>
-        <FileInput
-          ref={inputRef}
-          className={styles["file-input"]}
-          fileTypeIndicator={<FileSVG />}
-          inputDescription="Choose files"
-          acceptedFileTypes={ACCEPTED_FILES}
-        />
-        <p className="text-secondary">Or drop them here. Max size: 10MB.</p>
-      </div>
-    </section>
-  )
+	return (
+		<section className={styles["image-control"]}>
+			<div className={styles["image-control__file-container"]}>
+				<FileInput
+					ref={inputRef}
+					className={styles["file-input"]}
+					fileTypeIndicator={<FileSVG />}
+					inputDescription="Choose files"
+					acceptedFileTypes={ACCEPTED_FILES}
+				/>
+				<p className="text-secondary">Or drop them here. Max size: 10MB.</p>
+			</div>
+		</section>
+	)
 }
 
 export default ImageInputContainer
