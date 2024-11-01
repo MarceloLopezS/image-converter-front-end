e380350d3b4a2d7f0457f176d68cd1558c722757
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-01 15:05:56 -0500

    add: case for add-files action

--- src/shared/state/reducers/root/index.js
+++ src/shared/state/reducers/root/index.js
@@ -1,10 +1,12 @@
-import { TOGGLE_THEME } from "../../config/actions"
-import { toggleTheme } from "../../model/actionHandlers"
+import { TOGGLE_THEME, ADD_FILES } from "../../config/actions"
+import { toggleTheme, addFiles } from "../../model/actionHandlers"
 
 const rootReducer = (state, action) => {
   switch (action.type) {
     case TOGGLE_THEME:
       return toggleTheme(state)
+    case ADD_FILES:
+      return addFiles(state, action)
     default:
       console.error(`Unknown action: ${action.type}`)
       return state
