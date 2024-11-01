245d0c5cdfe4cd45d36d9868da56c838d8b19bad
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-01 14:58:41 -0500

    add: DeleteSVG

---
+++ src/shared/ui/SVGs/Delete/index.jsx
@@ -0,0 +1,22 @@
+const DeleteSVG = ({ ...attributes }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...attributes}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M10 10l4 4m0 -4l-4 4" />
+    </svg>
+  )
+}
+
+export default DeleteSVG
