9127b940e618646bd4eb168a729ac304ce191c2a
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-06 17:53:41 -0500

    update: replace html select logic with AllFilesOutputSelect component

--- src/widgets/FileControlSection/ui/ImageInputContainer/index.jsx
+++ src/widgets/FileControlSection/ui/ImageInputContainer/index.jsx
@@ -1,8 +1,5 @@
 import { dispatch, useStoreData } from "@shared/state/store"
-import {
-  ADD_FILES,
-  SET_FILES_OUTPUT_FORMAT
-} from "@shared/state/config/actions"
+import { ADD_FILES } from "@shared/state/config/actions"
 import { useInputValidationHandler } from "@shared/ui/Form"
 import {
   isFileInputValid,
@@ -12,22 +9,16 @@ import FileInput from "@shared/ui/FileInput"
 import FileSVG from "@shared/ui/SVGs/File"
 import styles from "./ui/styles.module.css"
 import File from "@entities/File"
-import { useRef } from "react"
+import AllFilesOutputSelect from "./ui/AllFilesOutputSelect"
 
 const ACCEPTED_FILES = ["image/png", "image/jpeg", "image/webp"]
 const OUTPUT_OPTIONS = ["png", "jpeg", "webp"]
 
 const ImageInputContainer = () => {
   const files = useStoreData(state => state.files)
-  const filesConfig = useStoreData(state => state.filesConfig)
   const fileInputHandler = useInputValidationHandler(
     isFileInputValid(ACCEPTED_FILES),
     handleInvalidFileInputMessage(ACCEPTED_FILES)
-  )
-  const selectOutputRef = useRef(null)
-
-  const outputFormats = new Set(
-    Object.values(filesConfig).map(fileConfig => fileConfig.outputFormat)
   )
 
   const handleFileChange = () => {
@@ -40,14 +31,6 @@ const ImageInputContainer = () => {
       payload: { selectedFiles: fileInputHandler.inputRef.current.files }
     })
     return
-  }
-
-  const handleOutputSelect = () => {
-    const filesOutputConfig = files.map(file => ({
-      fileName: file.name,
-      outputFormat: selectOutputRef.current.value
-    }))
-    dispatch({ type: SET_FILES_OUTPUT_FORMAT, payload: { filesOutputConfig } })
   }
 
   return (
@@ -80,23 +63,10 @@ const ImageInputContainer = () => {
               <label htmlFor="all-files-output">
                 All {`(${files.length})`} output:
               </label>
-              <select
-                ref={selectOutputRef}
-                onChange={handleOutputSelect}
-                id="all-files-output"
-                value={
-                  outputFormats.size === 1
-                    ? Array.from(outputFormats)[0] || "-"
-                    : "-"
-                }
-              >
-                <option disabled>-</option>
-                {OUTPUT_OPTIONS.map(option => (
-                  <option value={option} key={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+              <AllFilesOutputSelect
+                outputOptions={OUTPUT_OPTIONS}
+                fileNames={files.map(file => file.name)}
+              />
             </div>
             <button className={styles["file-control__submit"]} type="button">
               Convert
