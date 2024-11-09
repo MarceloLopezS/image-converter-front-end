103c1ce92935e916d85e2f0c41f67af7afb0195b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-08 19:04:55 -0500

    add: FileDropZone component

---
+++ src/shared/ui/FileDropZone/index.jsx
@@ -0,0 +1,78 @@
+import { useRef, useState } from "react"
+import FileInput from "@shared/ui/FileInput"
+
+const FileDropZone = ({
+  classNames = { container: "", containerOnDragOver: "", inputButton: "" },
+  acceptedMIMETypes = ["image"],
+  fileTypeIndicator = <></>,
+  filesValidateFn = files => true,
+  errorMessageHandler = files => "",
+  onFileChange = files => undefined
+}) => {
+  const fileInputRef = useRef(null)
+  const [isDragingOver, setIsDragingOver] = useState(false)
+  const [error, setError] = useState(null)
+
+  const onDragEnter = event => {
+    setIsDragingOver(true)
+  }
+
+  const onDragLeave = event => {
+    setIsDragingOver(false)
+  }
+
+  const onDrop = event => {
+    event.preventDefault()
+    const files = event.dataTransfer.items
+      ? [...event.dataTransfer.items]
+          .filter(item => item.kind === "file")
+          .map(item => item.getAsFile())
+      : [...event.dataTransfer.files]
+
+    if (!filesValidateFn(files)) return
+
+    onFileChange(files)
+  }
+
+  const onChange = () => {
+    const files = fileInputRef.current.files
+
+    if (!filesValidateFn(files)) {
+      setError(errorMessageHandler(files))
+    }
+
+    onFileChange(files)
+  }
+
+  return (
+    <section
+      className={`${
+        isDragingOver && classNames.containerOnDragOver
+      }${classNames.container ? " " + classNames.container : ""}`}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onDragOver={e => e.preventDefault()}
+      onDrop={onDrop}
+    >
+      {error && <div>{error}</div>}
+      {isDragingOver ? (
+        fileTypeIndicator
+      ) : (
+        <>
+          <FileInput
+            ref={fileInputRef}
+            className={classNames.inputButton}
+            fileTypeIndicator={fileTypeIndicator}
+            inputDescription="Choose files"
+            acceptedFileTypes={acceptedMIMETypes}
+            onChange={onChange}
+            multiple
+          />
+          <p className="text-secondary">Or drop them here. Max size: 10MB.</p>
+        </>
+      )}
+    </section>
+  )
+}
+
+export default FileDropZone
