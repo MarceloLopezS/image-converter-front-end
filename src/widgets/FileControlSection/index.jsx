cf1acdb629eac1eb3095ded9493b9dc0bab60c8d
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-11 17:54:56 -0500

    add: FileConfigControl to composition

--- src/widgets/FileControlSection/index.jsx
+++ src/widgets/FileControlSection/index.jsx
@@ -1,10 +1,12 @@
 import ImageInputContainer from "./ui/ImageInputContainer"
+import FileConfigControl from "./ui/FileConfigControl"
 import styles from "./ui/styles.module.css"
 
 const FileControlSection = () => {
   return (
     <section className={styles["file-control"]}>
       <ImageInputContainer />
+      <FileConfigControl />
     </section>
   )
 }
