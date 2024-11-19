e140af30cba9c8fd4d11b09c2f1354874ddeb313
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-19 14:34:23 -0500

    add: API_URL and API_ENDPOINT constants

--- src/shared/utils/constants/index.js
+++ src/shared/utils/constants/index.js
@@ -1,7 +1,11 @@
+export const API_URL = import.meta.env.VITE_API_URL
+export const API_ENDPOINT = {
+  OUTPUT_FORMAT_PARAMS: "/output-format-params"
+}
+export const INITIAL_FILE_PARAMS = {
+  outputFormat: null
+}
 export const THEME = "theme"
 export const DARK = "dark"
 export const LIGHT = "light"
 export const PREFERS_COLOR_SCHEME_DARK = `(prefers-color-scheme: ${DARK})`
-export const INITIAL_FILE_PARAMS = {
-  outputFormat: null
-}
