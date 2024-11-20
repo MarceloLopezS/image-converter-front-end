3dd05db0f2c345bb7fde3a8eed21ac9b7976b3d9
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-20 18:00:46 -0500

    add: react query client provider to app

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "./ui/global.css"
 import Home from "@pages/home"
 
+const queryClient = new QueryClient()
+
 const App = () => {
-  return <Home />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  )
 }
 
 export default App
