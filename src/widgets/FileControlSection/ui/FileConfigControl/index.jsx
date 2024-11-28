05fd4d3c1ad9bec7c29b486864f13591b4a8f125
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-27 20:35:42 -0500

    add: logic for file params config and display

--- src/widgets/FileControlSection/ui/FileConfigControl/index.jsx
+++ src/widgets/FileControlSection/ui/FileConfigControl/index.jsx
@@ -1,18 +1,40 @@
+import { useQuery } from "@tanstack/react-query"
 import { useStoreData } from "@shared/state/store"
+import { API_ENDPOINT } from "@shared/utils/constants"
 import { getFilesSettingsWarning } from "@shared/utils/functions"
+import Loader from "@shared/ui/Loader"
+import ParamsForm from "./ui/ParamsForm"
 import styles from "./ui/styles.module.css"
+
+const DEFAULT_ERROR_MESSAGE =
+  "There was an error during the request. Please try again later."
 
 const FileConfigControl = () => {
   const files = useStoreData(state => state.files)
   const filesConfig = useStoreData(state => state.filesConfig)
   const currentFileToConfig = useStoreData(state => state.currentFileToConfig)
 
+  const outputFormats = new Set(
+    Object.values(filesConfig).map(fileConfig => fileConfig.outputFormat)
+  )
+  const commonOutputFormat =
+    outputFormats.size === 1 ? Array.from(outputFormats)[0] : null
   const filesLength = files.length
   const warningMessage = getFilesSettingsWarning(
     filesLength,
     filesConfig,
     currentFileToConfig
   )
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: [
+      API_ENDPOINT.OUTPUT_FORMAT_PARAMS,
+      commonOutputFormat != null
+        ? commonOutputFormat
+        : filesConfig[currentFileToConfig]?.outputFormat
+    ],
+    enabled: !warningMessage
+  })
 
   return (
     <section className={styles["file-config-container"]}>
@@ -25,15 +47,24 @@ const FileConfigControl = () => {
         <p>
           {filesLength > 0
             ? (currentFileToConfig && `Settings for ${currentFileToConfig}:`) ||
-              `Settings for all (${filesLength}) files:`
+              `Preview settings for all (${filesLength}) files:`
             : "Settings:"}
         </p>
       </section>
       <section className={styles["file-config__body"]}>
         {warningMessage ? (
           <p className="text-secondary">{warningMessage}</p>
+        ) : isLoading ? (
+          <div className="grid place-items-center">
+            <Loader />
+          </div>
+        ) : error ? (
+          <p className="text-secondary">{DEFAULT_ERROR_MESSAGE}</p>
         ) : (
-          <section></section>
+          <ParamsForm
+            fileName={currentFileToConfig}
+            serverOutputParams={data?.data?.output_params}
+          />
         )}
       </section>
     </section>
