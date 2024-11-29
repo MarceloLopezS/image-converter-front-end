2ac2809c28e1ec2266f3e307dd3e2007a38550d0
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-29 13:13:06 -0500

    add: ALLOWED_IO prop to API_ENDPOINT constant

--- src/shared/utils/constants/index.js
+++ src/shared/utils/constants/index.js
@@ -1,5 +1,6 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const API_ENDPOINT = {
+  ALLOWED_IO: "/allowed-io",
   OUTPUT_FORMAT_PARAMS: "/output-format-params"
 }
 export const INITIAL_FILE_PARAMS = {
